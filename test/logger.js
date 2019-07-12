0754782b4da225a64aa80460c9076dad7ba606bd
Deepanshu Gupta <guptadeepanshu25@gmail.com>
2019-07-12 13:45:31 -0700

    remove unused lib

--- test/logger.js
+++ test/logger.js
@@ -5,10 +5,7 @@
  * For full license text, see LICENSE.txt file in the repo root or
  * https://opensource.org/licenses/BSD-3-Clause
  */
-const chai = require('chai');
-const chaiAsPromised = require('chai-as-promised');
-chai.use(chaiAsPromised);
-const expect = chai.expect;
+const expect = require('chai').expect;
 const { initKafkaLoggingProducer, writeLog } = require('../src/logger');
 const KafkaProducer = require('no-kafka');
 const sinon = require('sinon');
