9baa139ed0fa46ae9530e1468edcde3a6d1f4502
Deepanshu Gupta <guptadeepanshu25@gmail.com>
2019-07-15 15:12:29 -0700

    small fix

--- src/logger.js
+++ src/logger.js
@@ -58,7 +58,7 @@ const writeLog = (value, key = 'info', topic = config.topic, callback = console.
     partition: 0,
     message: {
       key,
-      value: messageValue,
+      value: JSON.stringify(messageValue),
     },
   };
   let promise;
