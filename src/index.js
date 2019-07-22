1a61b4bd5afc5a7a093e1472f7e0040afc3b3b5d
Deepanshu Gupta <guptadeepanshu25@gmail.com>
2019-07-22 13:12:41 -0700

    include toggles

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const whitelistUtils = require('./whitelistUtils');
 const expressUtils = require('./expressUtils');
 const port = process.env.PORT || 3000;
 const logger = require('./logger');
+require('./toggles');
 const listening = `Listening on port ${port}`;
 const API_CACHE_DURATION = process.env.API_CACHE_DURATION || false;
 
